220cafff92aca0554862365e635e896249b63ad7
백재우 <9712jw@gmail.com>
2025-08-27 17:52:35 +0900

    :tada: spring security 만들기 project init

---
+++ ch02/src/main/java/securitymake/Ch02.java
@@ -0,0 +1,12 @@
+package securitymake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch02 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch02.class, args);
+    }
+}
