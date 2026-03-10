91bb69330a62649f067df3b2be2ec62487284d58
jaewoo <9712jw@gmail.com>
2026-03-11 08:18:09 +0900

    chapter05 모듈 생성

---
+++ chapter5/src/main/java/securitymake/Chapter05.java
@@ -0,0 +1,12 @@
+package securitymake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter05 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter05.class, args);
+    }
+}
