ca4939371489e789cb7cf7ed853bbd1a89a32cd4
jaewoo <9712jw@gmail.com>
2026-03-13 13:27:17 +0900

    카카오 로그인 client registration 설정 추가

---
+++ chapter12-kakao/src/main/java/securitymake/Chapter12Kakao.java
@@ -0,0 +1,11 @@
+package securitymake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter12Kakao {
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter12Kakao.class);
+	}
+}
--- chapter12-kakao/src/main/java/securitymake/controller/MainController.java
+++ chapter12-kakao/src/main/java/securitymake/controller/MainController.java
@@ -1,4 +1,4 @@
-package controller;
+package securitymake.controller;
 
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
