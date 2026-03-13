2d1bcb7f6271ad9f1e63dbf5cf2ef1f0f87c09e2
jaewoo <9712jw@gmail.com>
2026-03-13 09:35:32 +0900

    기존 client Registration 을 application 설정 파일로 리팩토링

--- chapter12/src/main/java/securitymake/config/ProjectConfig.java
+++ chapter12/src/main/java/securitymake/config/ProjectConfig.java
@@ -2,12 +2,9 @@ package securitymake.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,24 +12,9 @@ import org.springframework.security.web.SecurityFilterChain;
 public class ProjectConfig {
 
 	@Bean
-	public ClientRegistrationRepository clientRepository() {
-		return new InMemoryClientRegistrationRepository(this.clientRegistration());
-	}
-
-	private ClientRegistration clientRegistration() {
-		return CommonOAuth2Provider.GITHUB
-			.getBuilder("github")
-			.clientId("Iv23li4jvPlAEIH1ulOu")
-			.clientSecret("790611e93c38b1dc051c648871aa6496757cca59")
-			.build();
-	}
-
-	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.oauth2Login((oauth2) -> oauth2
-				.clientRegistrationRepository(clientRepository())
-			);
+			.oauth2Login(Customizer.withDefaults());
 
 		http
 			.authorizeHttpRequests(auth ->
